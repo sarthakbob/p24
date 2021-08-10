a7cb32d3edf06c357d79429ee124d8fb72700d0a
sarthakbob <70557482+sarthakbob@users.noreply.github.com>
2021-08-09 18:47:10 -0700

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,26 @@
+class paper {
+    constructor(x,y,r){
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.body = Bodies.circle(this.x,this.y,this.r,options);
+        World.add(world,this.body);
+
+    }
+
+    display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(this.body.angle)
+        imageMode(CENTER);
+        ellipse(0,0,40,40);
+        pop();
+    }
+}
